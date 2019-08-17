9cdf4f8b150df418d7ddc37dc37c8cda4d3281b7
leetking <li_tking@163.com>
2019-08-17 18:02:32 +0800

    Add a post.

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+/*
+(function() {
+    document.addEventListener('loadend', function(_) {
+        document.querySelector('#author-name').addEventListener('mouseover', function(event) {
+            event.target.innerText = '0_o';
+        });
+    });
+})();
+*/
